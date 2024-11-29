222c18e756fe3af10aaa7b6a73fdc36b9922e8c6
Luciana Moura <lu.moura@gmail.com>
2024-11-29 19:36:43 -0300

    test(AppComponent): verifica h1 do header dentro do appComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,8 +10,17 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('deve renderizar o componente header', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+    const headerdebugEl = fixture.debugElement.query(By.css('app-header'));
+    const text = headerdebugEl.query(By.css('h1')).nativeElement.textContent;
+    expect(text).toBe('Gerenciador de Tarefas');
+    //expect(headerdebugEl).toBeTruthy();
+  });
+
+  it('deve renderizar o componente router-outlet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const routerOutletdebugEl = fixture.debugElement.query(By.css('router-outlet'));
+    expect(routerOutletdebugEl).toBeTruthy();
   });
 });
