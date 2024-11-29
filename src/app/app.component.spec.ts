34eaecdb56e33ffeb09934e47457d297ab06d1e5
Luciana Moura <lu.moura@gmail.com>
2024-11-29 19:50:26 -0300

    test(AppComponent): cria teste

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,20 +2,36 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  template: ''
+})
+class FakeHeaderComponent {}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, RouterTestingModule],
     }).compileComponents();
+
+    TestBed.overrideComponent(AppComponent, {
+      remove: {
+        imports: [HeaderComponent]
+      },
+      add: {
+        imports: [FakeHeaderComponent]  
+      }
+    })
   });
 
   it('deve renderizar o componente header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const headerdebugEl = fixture.debugElement.query(By.css('app-header'));
-    const text = headerdebugEl.query(By.css('h1')).nativeElement.textContent;
-    expect(text).toBe('Gerenciador de Tarefas');
-    //expect(headerdebugEl).toBeTruthy();
+    expect(headerdebugEl).toBeTruthy();
   });
 
   it('deve renderizar o componente router-outlet', () => {
