d88151a5db3c76c559a9e1947be7241eefb06e55
Luciana Moura <lu.moura@gmail.com>
2024-12-02 15:41:43 -0300

    feat(ListComponent): utiliza TasksService no componente

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,28 +1,24 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-interface Task {
-  title: string;
-  completed: boolean;
-}
+import { Task } from 'src/app/shared/interfaces/tasks.interface';
+import { TasksService } from 'src/app/shared/services/tasks/tasks.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './list.component.html',
+templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
 
-  tasks = signal<Task[]>([
-    { title: 'Tarefa 1', completed: false },
-    { title: 'Tarefa 2', completed: true },
-    { title: 'Tarefa 3', completed: false },
-    { title: 'Tarefa 4', completed: true },
-    { title: 'Tarefa 5', completed: false },
-    { title: 'Tarefa 6', completed: true },
-  ]);
+  tasksService = inject(TasksService);
+  tasks = signal<Task[]>([]);
 
   pending = computed(() => this.tasks().filter(task => !task.completed));
   completedTasks = computed(() => this.tasks().filter(task => task.completed));
+
+  ngOnInit(): void {
+    this.tasksService.getAll().subscribe(tasks => this.tasks.set(tasks));
+  }
 }
