900b70e9a3a223d6558ec8f5e9fa2df90734a386
Luciana Moura <lu.moura@gmail.com>
2024-12-02 15:42:44 -0300

    test((ListComponent): utiliza TasksService no teste

--- src/app/pages/list/list.component.spec.ts
+++ src/app/pages/list/list.component.spec.ts
@@ -1,22 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
 import { By } from '@angular/platform-browser';
+import { TasksService } from 'src/app/shared/services/tasks/tasks.service';
+import { of } from 'rxjs';
+
+class FakeTasksService implements TasksService {
+  getAll = jest.fn();
+}
 
 describe('ListComponent', () => {
-  let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let tasksService: TasksService;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    TestBed.configureTestingModule({
       imports: [ListComponent],
-    }).compileComponents();
+      providers: [
+        {
+          provide: TasksService,
+          useClass: FakeTasksService
+        }
+      ]
+    })
+    
+    await TestBed.compileComponents();
 
-    fixture = TestBed.createComponent(ListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    tasksService = TestBed.inject(TasksService);
   });
 
   it('deve listar as tarefas', () => {
+
+    (tasksService.getAll as jest.Mock).mockReturnValue(of([
+      { title: 'Tarefa 1', completed: false },
+      { title: 'Tarefa 2', completed: true },
+      { title: 'Tarefa 3', completed: false },
+      { title: 'Tarefa 4', completed: true },
+      { title: 'Tarefa 5', completed: false },
+      { title: 'Tarefa 6', completed: true }
+    ]));
+
+    fixture = TestBed.createComponent(ListComponent);
+    fixture.detectChanges();
 
     const todoSection = fixture.debugElement.query(By.css('[data-testid="todo-list"]'));
 
