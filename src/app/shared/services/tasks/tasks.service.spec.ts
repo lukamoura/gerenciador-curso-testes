841084b97b1091699e12031d3cb33f8e1fce2548
Luciana Rodrigues <lu.moura@gmail.com>
2024-12-04 15:47:06 -0300

    feat(TasksService): testa a chamada http de getAll

--- src/app/shared/services/tasks/tasks.service.spec.ts
+++ src/app/shared/services/tasks/tasks.service.spec.ts
@@ -1,13 +1,23 @@
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { TasksService } from './tasks.service';
 import { Task } from '../../interfaces/tasks.interface';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('TasksService', () => {
   let service: TasksService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(TasksService);
+
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('getTasks() deve retornar uma lista de tarefas', fakeAsync(() => {
@@ -18,14 +28,20 @@ describe('TasksService', () => {
       result = tasks;
     })
 
-    tick();
-    expect(result).toEqual([
+    const request = httpTestingController.expectOne('/tasks');
+
+    const fakeTasks: Task[] = [
       { title: 'Tarefa 1', completed: false },
       { title: 'Tarefa 2', completed: true },
       { title: 'Tarefa 3', completed: false },
       { title: 'Tarefa 4', completed: true },
       { title: 'Tarefa 5', completed: false },
       { title: 'Tarefa 6', completed: true }
-    ]);
+    ]
+
+    request.flush(fakeTasks);
+
+    tick();
+    expect(result).toEqual(fakeTasks);
   }));
 });
