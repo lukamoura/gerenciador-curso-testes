92983ec7804165951f2886ad1380ea7175414429
Luciana Rodrigues <lu.moura@gmail.com>
2024-12-11 11:28:38 -0300

    feat(NoItemsComponent): cria componente

---
+++ src/app/pages/list/no-items/no-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './no-items.component.html',
+})
+export class NoItemsComponent {
+
+}
