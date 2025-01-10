e4bd9514162f225c75f52c97a94ee488740f8c45
Luciana Rodrigues <lu.moura@gmail.com>
2025-01-10 11:25:46 -0300

    test(ListItemComponent): implementa teste com setup function

--- src/app/pages/list/list-item/list-item.component.spec.ts
+++ src/app/pages/list/list-item/list-item.component.spec.ts
@@ -1,30 +1,68 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { ListItemComponent } from './list-item.component';
 import { Task } from 'src/app/shared/interfaces/tasks.interface';
 import { TestHelper } from '@testing/helpers/text-helper';
+import { Component } from '@angular/core';
+
+async function setup(fakeTask: Task) {
+  
+  @Component({
+    standalone: true,
+    imports: [ListItemComponent],
+    template: `<app-list-item 
+      [task]="task" 
+      (complete)="onCompleteTask($event)"
+      ></app-list-item>`,
+  })
+  class HostComponent {
+    task = fakeTask;
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onCompleteTask() {
+
+    }
+  }
+
+  await TestBed.configureTestingModule({
+    imports: [HostComponent],
+  }).compileComponents();
+
+  const fixture = TestBed.createComponent(HostComponent);
+  const testHelper = new TestHelper(fixture);
+
+  return {fixture, testHelper};
+}
 
 describe('ListItemComponent', () => {
-  let fixture: ComponentFixture<ListItemComponent>;
-  let testHelper: TestHelper<ListItemComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ListItemComponent],
-    })
-.compileComponents();
-
-    fixture = TestBed.createComponent(ListItemComponent);
-    testHelper = new TestHelper(fixture);
-  });
-
-  it('deve renderizar o título da tarefa', () => {
+  it('deve renderizar o título da tarefa', async() => {
     const fakeTask: Task = {
       title: 'Item 1',
       completed: false
     };
-    fixture.componentRef.setInput('task', fakeTask);
+
+    const {fixture, testHelper} = await setup(fakeTask);
+
     fixture.detectChanges();
     const textContent = testHelper.getTextContentByTestId("list-item-task-title");
     expect(textContent).toBe(fakeTask.title);
+  })
+
+  it('deve emitir um evento ao completar a tarefa', async () => {
+
+    const fakeTask: Task = {
+      title: 'Item 1',
+      completed: false
+    };
+
+    const {fixture, testHelper} = await setup(fakeTask);
+
+    const onCompleteTaskSpy = jest.spyOn(fixture.componentInstance, 'onCompleteTask');
+    
+    fixture.detectChanges();
+
+    const completeBtnDebugEl = testHelper.queryByTestId('list-item-complete-action');
+    completeBtnDebugEl.triggerEventHandler('click', null);
+
+    expect(onCompleteTaskSpy).toHaveBeenCalled();
   });
 });
--- src/testing/mocks/fake-list-item.component.ts
+++ src/testing/mocks/fake-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, InputSignal } from "@angular/core";
+import { Component, input, InputSignal, output } from "@angular/core";
 import { ListItemComponent } from "src/app/pages/list/list-item/list-item.component";
 import { Task } from "src/app/shared/interfaces/tasks.interface";
 
@@ -8,5 +8,13 @@ import { Task } from "src/app/shared/interfaces/tasks.interface";
     template: ''
 })
 export class FakeListItemComponent implements ListItemComponent {
+
+    complete = output<Task>();
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onComplete(): void {
+
+    }
+
     task = input.required<Task>();
 }
