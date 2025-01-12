8c02dc30c78deae2aba7edf3b0ff9ce56c8b4539
Luciana Rodrigues <lu.moura@gmail.com>
2025-01-12 12:48:18 -0300

    test(FakeTasksService): adiciona método patch

--- src/testing/mocks/fake-tasks.service.ts
+++ src/testing/mocks/fake-tasks.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Task } from "src/app/shared/interfaces/tasks.interface";
 import { TasksService } from "src/app/shared/services/tasks/tasks.service";
 
 export class FakeTasksService implements TasksService {
     httpClient!: HttpClient;  
     getAll = jest.fn();
+    patch = jest.fn();
   }
