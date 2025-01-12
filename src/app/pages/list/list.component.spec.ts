129e1d24e35d581957ec3ac97881f86c30b52efa
Luciana Rodrigues <lu.moura@gmail.com>
2025-01-12 12:49:27 -0300

    test(ListComponent): adiciona teste para a ação de completar tarefa

--- src/app/pages/list/list.component.spec.ts
+++ src/app/pages/list/list.component.spec.ts
@@ -6,10 +6,13 @@ import { of } from 'rxjs';
 import { FakeTasksService } from "@testing/mocks/fake-tasks.service";
 import { ListItemComponent } from './list-item/list-item.component';
 import { FakeListItemComponent } from "@testing/mocks/fake-list-item.component";
+import { Task } from 'src/app/shared/interfaces/tasks.interface';
+import { TestHelper } from '@testing/helpers/text-helper';
 
 describe('ListComponent', () => {
   let fixture: ComponentFixture<ListComponent>;
   let tasksService: TasksService;
+  let testHelper: TestHelper<ListComponent>;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
@@ -33,18 +36,21 @@ describe('ListComponent', () => {
     
     await TestBed.compileComponents();
 
+    fixture = TestBed.createComponent(ListComponent);
+    testHelper = new TestHelper(fixture);
+
     tasksService = TestBed.inject(TasksService);
   });
 
   it('deve listar as tarefas', () => {
 
     (tasksService.getAll as jest.Mock).mockReturnValue(of([
-      { title: 'Tarefa 1', completed: false },
-      { title: 'Tarefa 2', completed: true },
-      { title: 'Tarefa 3', completed: false },
-      { title: 'Tarefa 4', completed: true },
-      { title: 'Tarefa 5', completed: false },
-      { title: 'Tarefa 6', completed: true }
+      { id: '1', title: 'Tarefa 1', completed: false },
+      { id: '2', title: 'Tarefa 2', completed: true },
+      { id: '3', title: 'Tarefa 3', completed: false },
+      { id: '4', title: 'Tarefa 4', completed: true },
+      { id: '5', title: 'Tarefa 5', completed: false },
+      { id: '6', title: 'Tarefa 6', completed: true }
     ]));
 
     fixture = TestBed.createComponent(ListComponent);
@@ -73,5 +79,30 @@ describe('ListComponent', () => {
     expect(completedItems[0].componentInstance.task()).toEqual({ title: 'Tarefa 2', completed: true });
     expect(completedItems[1].componentInstance.task()).toEqual({ title: 'Tarefa 4', completed: true });
     expect(completedItems[2].componentInstance.task()).toEqual({ title: 'Tarefa 6', completed: true });
+  });
+
+  describe('quando a tarefa está pendente', () => {
+    it('deve completar a tarefa', () => {
+
+      const fakeTask: Task = { id: '1', title: 'Tarefa 1', completed: false };
+      const fakeTasks: Task[] = [fakeTask];
+
+      (tasksService.getAll as jest.Mock).mockReturnValue(of(fakeTasks));
+
+      const completedTask = { ...fakeTask, completed: true };
+
+      (tasksService.patch as jest.Mock).mockReturnValue(of(completedTask));
+
+      fixture.detectChanges();
+
+      const todoDebugEl = testHelper.queryByTestId('todo-list-item');
+      (todoDebugEl.componentInstance as FakeListItemComponent).complete.emit(fakeTask);
+
+      expect(tasksService.patch).toHaveBeenCalledWith(fakeTask.id, { completed: true });
+
+      fixture.detectChanges();
+      
+      expect(testHelper.queryByTestId('completed-list-item')).toBeTruthy();
+    });
   });
 });
