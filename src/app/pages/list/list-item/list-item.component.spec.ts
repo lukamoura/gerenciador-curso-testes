72b43480639e2d440690adfd3f6596b8e37a3a11
Luciana Rodrigues <lu.moura@gmail.com>
2025-01-14 21:38:46 -0300

    test(ListComponent): testa ação de remover tarefa

--- src/app/pages/list/list-item/list-item.component.spec.ts
+++ src/app/pages/list/list-item/list-item.component.spec.ts
@@ -13,6 +13,7 @@ async function setup(fakeTask: Task) {
       [task]="task" 
       (complete)="onCompleteTask($event)"
       (notComplete)="onNotComplete($event)"
+      (remove)="onRemove($event)"
       ></app-list-item>`,
   })
   class HostComponent {
@@ -23,6 +24,9 @@ async function setup(fakeTask: Task) {
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     onNotComplete() {}
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onRemove(task: Task) {}
   }
 
   await TestBed.configureTestingModule({
@@ -90,6 +94,24 @@ describe('ListItemComponent', () => {
   
       expect(onCompleteTaskSpy).toHaveBeenCalled();
     });
+
+    it('deve emitir um evento ao de remover tarefa', async () => {
+      const fakeTask: Task = {
+        id: '1',
+        title: 'Item 1',
+        completed: false
+      };
+  
+      const {fixture, testHelper} = await setup(fakeTask);
+  
+      const onRemoveTaskSpy = jest.spyOn(fixture.componentInstance, 'onRemove');
+      
+      fixture.detectChanges();
+  
+      testHelper.click('list-item-remove-action');
+
+      expect(onRemoveTaskSpy).toHaveBeenCalledWith(fakeTask);
+    });
   });  
 
   describe('quando a tarefa estiver concluída', () => {
@@ -133,6 +155,24 @@ describe('ListItemComponent', () => {
       markAsPendingDebugEl.triggerEventHandler('click', null);
   
       expect(onNotCompleteSpy).toHaveBeenCalled();
+    });
+
+    it('deve emitir um evento ao de remover tarefa', async () => {
+      const fakeTask: Task = {
+        id: '1',
+        title: 'Item 1',
+        completed: true
+      };
+  
+      const {fixture, testHelper} = await setup(fakeTask);
+  
+      const onRemoveTaskSpy = jest.spyOn(fixture.componentInstance, 'onRemove');
+      
+      fixture.detectChanges();
+  
+      testHelper.click('list-item-remove-action');
+
+      expect(onRemoveTaskSpy).toHaveBeenCalledWith(fakeTask);
     });
   });  
 });
--- src/testing/helpers/text-helper.ts
+++ src/testing/helpers/text-helper.ts
@@ -4,11 +4,16 @@ import { By } from '@angular/platform-browser';
 export class TestHelper<T> {
     constructor(private fixture: ComponentFixture<T>) {}
 
-    queryByTestId(testId: string) {
-        return this.fixture.debugElement.query(By.css(`[data-testid="${testId}"]`));
-    }
-
     getTextContentByTestId(testId: string) {
         return this.queryByTestId(testId).nativeElement.textContent.trim();
+    }
+
+    click(testId: string) {
+        this.queryByTestId(testId).nativeElement.click();
+        this.fixture.detectChanges();
+    }
+
+    queryByTestId(testId: string) {
+        return this.fixture.debugElement.query(By.css(`[data-testid="${testId}"]`));
     }
 }
