4470f7f6e7e25aad91e34c5ae7e6fc1390ffc30b
Luciana Rodrigues <lu.moura@gmail.com>
2025-01-21 18:32:03 -0300

    feat: cria roteamento CreatTaskComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const appRoutes: Route[] = [
     {
         path: '',
         loadComponent: () => import('./pages/list/list.component').then(m => m.ListComponent)
+    },
+    {
+        path: 'create',
+        loadComponent: () => import('./pages/create-task/create-task.component').then(m => m.CreateTaskComponent)
     }
 ];
