2d23f3364b6a5bf7a8ea83ca035db2f758f51313
Luciana Rodrigues <lu.moura@gmail.com>
2025-01-21 18:31:12 -0300

    test(CreateTaskComponent): testa componente

---
+++ src/app/pages/create-task/create-task.component.spec.ts
@@ -0,0 +1,59 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CreateTaskComponent } from './create-task.component';
+import { TestHelper } from '@testing/helpers/text-helper';
+import { TasksService } from 'src/app/shared/services/tasks/tasks.service';
+import { FakeTasksService } from '@testing/mocks/fake-tasks.service';
+import { Router } from '@angular/router';
+import { Task, TaskWithoutId } from 'src/app/shared/interfaces/tasks.interface';
+import { of } from 'rxjs';
+
+describe('CreateTaskComponent', () => {
+  let fixture: ComponentFixture<CreateTaskComponent>;
+  let testHelper: TestHelper<CreateTaskComponent>;
+  let tasksService: TasksService;
+  let router: Router;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CreateTaskComponent],
+      providers: [{
+        provide: TasksService,
+        useClass: FakeTasksService,
+      }],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CreateTaskComponent);
+    testHelper = new TestHelper(fixture);
+
+    tasksService = TestBed.inject(TasksService);
+    router = TestBed.inject(Router);
+
+    fixture.detectChanges();
+  });
+
+  it('deve criar uma tarefa', () => {
+    const fakeTaskTitle = 'Fake Task Title';
+
+    const fakeTaskPayload: TaskWithoutId = {title: fakeTaskTitle, completed: false};
+
+    const fakeTaskResponse: Task = {id: '1', ...fakeTaskPayload};
+
+    (tasksService.post as jest.Mock).mockReturnValue(of(fakeTaskResponse));
+
+    const navigateByUrlSpy = jest.spyOn(router, 'navigateByUrl');
+
+    const titleDebugEl = testHelper.queryByTestId('create-task-title');
+
+    titleDebugEl.triggerEventHandler('input', { target: { value: fakeTaskTitle } });
+
+    fixture.detectChanges();   
+
+    const formDebugEl = testHelper.queryByTestId('create-task-form');
+    
+    formDebugEl.triggerEventHandler('submit', null); 
+   
+    expect(tasksService.post).toHaveBeenCalledWith(fakeTaskPayload);
+
+    expect(navigateByUrlSpy).toHaveBeenCalledWith('/');
+  });
+});
